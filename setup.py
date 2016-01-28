c09004ed7e100fb4b7f5fe44f94e799a11539b90
Brandon Castellano <brandon248@gmail.com>
2016-01-27 19:52:08 -0500

    Updated PyPI packages and updated development status to production to remove necessity of --pre flag while installing.

--- setup.py
+++ setup.py
@@ -43,17 +43,23 @@ setup(
     #test_suite="unitest.py",
     entry_points={"console_scripts": ["scenedetect=scenedetect:main"]},
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Environment :: Console :: Curses',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Topic :: Multimedia :: Video :: Non-Linear Editor',
+        'Topic :: Utilities'
     ]
 )
