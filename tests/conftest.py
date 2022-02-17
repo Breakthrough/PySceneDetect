be21105cf56414c35d243155b196985b77901f28
Brandon Castellano <brandon248@gmail.com>
2022-02-16 22:19:09 -0500

    Fix build.

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,6 +32,7 @@ These tests rely on the files in the tests/resources/ folder in the "resources" 
 the PySceneDetect git repository. These files can be checked out via git by running the
 following from the root of the repo:
 
+    git fetch --depth=1 https://github.com/Breakthrough/PySceneDetect.git refs/heads/resources:refs/remotes/origin/resources
     git checkout refs/remotes/origin/resources -- tests/resources/
 
 Note that currently these tests create some temporary files which are not yet cleaned up.
