b62a6cdac3df86add4fe90ebfb3e072f41fedf51
Brandon Castellano <brandon248@gmail.com>
2022-02-16 21:26:09 -0500

    Add type hints and reformat scenedetect.scene_detector.

--- scenedetect/scene_detector.py
+++ scenedetect/scene_detector.py
@@ -18,7 +18,6 @@
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-
 """ ``scenedetect.scene_detector`` Module
 
 This module implements the base SceneDetector class, from which all scene
@@ -28,9 +27,14 @@ The SceneDetector class represents the interface which detection algorithms
 are expected to provide in order to be compatible with PySceneDetect.
 """
 
+from typing import List, Optional, Tuple
+
+import numpy
+
+from scenedetect.stats_manager import StatsManager
+
+
 # pylint: disable=unused-argument, no-self-use
-
-
 class SceneDetector:
     """ Base class to inherit from when implementing a scene detection algorithm.
 
@@ -41,16 +45,15 @@ class SceneDetector:
     to get an idea of how a particular detector can be created.
     """
 
-    stats_manager = None
-    """ Optional :py:class:`StatsManager <scenedetect.stats_manager.StatsManager>` to
+    stats_manager: Optional[StatsManager] = None
+    """Optional :py:class:`StatsManager <scenedetect.stats_manager.StatsManager>` to
     use for caching frame metrics to and from."""
 
-    def is_processing_required(self, frame_num):
-        # type: (int) -> bool
-        """ Is Processing Required: Test if all calculations for a given frame are already done.
+    def is_processing_required(self, frame_num: int) -> bool:
+        """Is Processing Required: Test if all calculations for a given frame are already done.
 
         Returns:
-            bool: False if the SceneDetector has assigned _metric_keys, and the
+            False if the SceneDetector has assigned _metric_keys, and the
             stats_manager property is set to a valid StatsManager object containing
             the required frame metrics/calculations for the given frame - thus, not
             needing the frame to perform scene detection.
@@ -59,58 +62,49 @@ class SceneDetector:
             to be passed to process_frame for the given frame_num).
         """
         metric_keys = self.get_metrics()
-        return not metric_keys or not (
-            self.stats_manager is not None and
-            self.stats_manager.metrics_exist(frame_num, metric_keys))
+        return not metric_keys or not (self.stats_manager is not None
+                                       and self.stats_manager.metrics_exist(frame_num, metric_keys))
 
-
-    def stats_manager_required(self):
-        # type: () -> bool
-        """ Stats Manager Required: Prototype indicating if detector requires stats.
+    def stats_manager_required(self) -> bool:
+        """Stats Manager Required: Prototype indicating if detector requires stats.
 
         Returns:
-            bool: True if a StatsManager is required for the detector, False otherwise.
+            True if a StatsManager is required for the detector, False otherwise.
         """
         return False
 
-
-    def get_metrics(self):
-        # type: () -> List[str]
-        """ Get Metrics:  Get a list of all metric names/keys used by the detector.
+    def get_metrics(self) -> List[str]:
+        """Get Metrics:  Get a list of all metric names/keys used by the detector.
 
         Returns:
-            List[str]: A list of strings of frame metric key names that will be used by
+            List of strings of frame metric key names that will be used by
             the detector when a StatsManager is passed to process_frame.
         """
         return []
 
-
-    def process_frame(self, frame_num, frame_img):
-        # type: (int, numpy.ndarray) -> List[int]
-        """ Process Frame: Computes/stores metrics and detects any scene changes.
+    def process_frame(self, frame_num: int, frame_img: numpy.ndarray) -> List[int]:
+        """Process Frame: Computes/stores metrics and detects any scene changes.
 
         Prototype method, no actual detection.
 
         Returns:
-            List[int]: List of frame numbers of cuts to be added to the cutting list.
+            List of frame numbers of cuts to be added to the cutting list.
         """
         return []
 
-
-    def post_process(self, frame_num):
-        # type: (int) -> List[int]
-        """ Post Process: Performs any processing after the last frame has been read.
+    def post_process(self, frame_num: int) -> List[int]:
+        """Post Process: Performs any processing after the last frame has been read.
 
         Prototype method, no actual detection.
 
         Returns:
-            List[int]: List of frame numbers of cuts to be added to the cutting list.
+            List of frame numbers of cuts to be added to the cutting list.
         """
         return []
 
 
 class SparseSceneDetector(SceneDetector):
-    """ Base class to inheret from when implementing a sparse scene detection algorithm.
+    """Base class to inheret from when implementing a sparse scene detection algorithm.
 
     Unlike dense detectors, sparse detectors detect "events" and return a *pair* of frames,
     as opposed to just a single cut.
@@ -118,27 +112,24 @@ class SparseSceneDetector(SceneDetector):
     An example of a SparseSceneDetector is the MotionDetector.
     """
 
-    def process_frame(self, frame_num, frame_img):
-        # type: (int, numpy.ndarray) -> List[Tuple[int, int]]
-        """ Process Frame: Computes/stores metrics and detects any scene changes.
+    def process_frame(self, frame_num: int, frame_img: numpy.ndarray) -> List[Tuple[int, int]]:
+        """Process Frame: Computes/stores metrics and detects any scene changes.
 
         Prototype method, no actual detection.
 
         Returns:
-            List[Tuple[int,int]]: List of frame pairs representing individual scenes
+            List of frame pairs representing individual scenes
             to be added to the output scene list directly.
         """
         return []
 
-
-    def post_process(self, frame_num):
-        # type: (int) -> List[Tuple[int, int]]
-        """ Post Process: Performs any processing after the last frame has been read.
+    def post_process(self, frame_num: int) -> List[Tuple[int, int]]:
+        """Post Process: Performs any processing after the last frame has been read.
 
         Prototype method, no actual detection.
 
         Returns:
-            List[Tuple[int,int]]: List of frame pairs representing individual scenes
+            List of frame pairs representing individual scenes
             to be added to the output scene list directly.
         """
         return []
