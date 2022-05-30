38bdb69cb92c49d77cbf834c20b5261b0b3d2459
Brandon Castellano <brandon248@gmail.com>
2022-05-29 22:48:18 -0400

    [build] Add missing setup.py from revert.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#         PySceneDetect: Python-Based Video Scene Detector
+#   ---------------------------------------------------------------
+#     [  Site:   http://www.scenedetect.scenedetect.com/         ]
+#     [  Docs:   http://manual.scenedetect.scenedetect.com/      ]
+#     [  Github: https://github.com/Breakthrough/PySceneDetect/  ]
+#
+# Copyright (C) 2014-2022 Brandon Castellano <http://www.bcastell.com>.
+# PySceneDetect is licensed under the BSD 3-Clause License; see the
+# included LICENSE file, or visit one of the above pages for details.
+#
+
+""" PySceneDetect setup.py """
+
+# TODO: Remove this. https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+from setuptools import setup
+setup(
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
