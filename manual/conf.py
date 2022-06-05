be2fbdbbfd9721d08d72c831712cb0723526b043
Brandon Castellano <brandon248@gmail.com>
2022-06-05 11:38:20 -0400

    [docs] Add extra whitespace to improve readability.

--- manual/conf.py
+++ manual/conf.py
@@ -96,6 +96,7 @@ pygments_style = 'sphinx'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files  = ['pyscenedetect.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
