95d7dea6d5a91b41fd161c7770075f1c66ca3138
Brandon Castellano <brandon248@gmail.com>
2022-06-09 21:26:17 -0400

    [build] Fix invalid version number.

--- scenedetect/__init__.py
+++ scenedetect/__init__.py
@@ -52,7 +52,7 @@ from scenedetect.video_manager import VideoManager
 
 # Used for module identification and when printing version & about info
 # (e.g. calling `scenedetect version` or `scenedetect about`).
-__version__ = 'v0.6.1-dev'
+__version__ = 'v0.6.1-dev0'
 # About & copyright message string shown for the 'about' CLI command (scenedetect about).
 
 ABOUT_STRING = """
