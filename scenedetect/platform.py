f41bc08b141ed6488add1ccb34671f6ddaf28ba3
Breakthrough <brandon248@gmail.com>
2024-01-28 21:55:36 -0500

    [docs] Document empty exception. Fixes #334

--- scenedetect/platform.py
+++ scenedetect/platform.py
@@ -245,12 +245,14 @@ def get_ffmpeg_path() -> Optional[str]:
     """Get path to ffmpeg if available on the current system. First looks at PATH, then checks if
     one is available from the `imageio_ffmpeg` package. Returns None if ffmpeg couldn't be found.
     """
+    # Try invoking ffmpeg with the current environment.
     try:
         subprocess.call(['ffmpeg', '-v', 'quiet'])
         return 'ffmpeg'
     except OSError:
-        pass
-    # Failed to invoke ffmpeg from PATH, see if we have a copy from imageio_ffmpeg.
+        pass  # Failed to invoke ffmpeg with current environment, try another possibility.
+
+    # Try invoking ffmpeg using the one from `imageio_ffmpeg` if available.
     try:
         # pylint: disable=import-outside-toplevel
         from imageio_ffmpeg import get_ffmpeg_exe
@@ -266,6 +268,7 @@ def get_ffmpeg_path() -> Optional[str]:
     # get_ffmpeg_exe may throw a RuntimeError if the executable is not available.
     except RuntimeError:
         pass
+
     return None
 
 
@@ -289,6 +292,7 @@ def get_mkvmerge_version() -> Optional[str]:
     try:
         output = subprocess.check_output(args=[tool_name, '--version'], text=True)
     except FileNotFoundError:
+        # mkvmerge doesn't exist on the system
         return None
     output_split = output.split()
     if len(output_split) >= 1 and output_split[0] == tool_name:
