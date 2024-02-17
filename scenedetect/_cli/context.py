Breakthrough/PySceneDetect

commit 46e170e02a2eea09808619a8d004852123a651c5

[config] Add `display-scenes` and `display-cuts` option to config file under `[list-scenes]`
Allows controlling which fields are displayed.

scenedetect/_cli/context.py
@@ -152,11 +152,13 @@ class CliContext:
 
         # `list-scenes` Command Options
         self.list_scenes: bool = False
-        self.print_scene_list: bool = None      # list-scenes -q/--quiet
+        self.list_scenes_quiet: bool = None     # list-scenes -q/--quiet
         self.scene_list_directory: str = None   # list-scenes -o/--output
         self.scene_list_name_format: str = None # list-scenes -f/--filename
         self.scene_list_output: bool = None     # list-scenes -n/--no-output
         self.skip_cuts: bool = None             # list-scenes -s/--skip-cuts
+        self.display_cuts: bool = True          # [list-scenes] display-cuts
+        self.display_scenes: bool = True        # [list-scenes] display-scenes
 
         # `export-html` Command Options
         self.export_html: bool = False
@@ -473,20 +475,22 @@ class CliContext:
         """Handle `list-scenes` command options."""
         self._ensure_input_open()
         if self.list_scenes:
-            self._on_duplicate_command('list-scenes')
+            self._on_duplicate_command("list-scenes")
 
-        self.skip_cuts = skip_cuts or self.config.get_value('list-scenes', 'skip-cuts')
-        self.print_scene_list = not (quiet or self.config.get_value('list-scenes', 'quiet'))
-        no_output_file = no_output_file or self.config.get_value('list-scenes', 'no-output-file')
+        self.display_cuts = self.config.get_value("list-scenes", "display-cuts")
+        self.display_scenes = self.config.get_value("list-scenes", "display-scenes")
+        self.skip_cuts = skip_cuts or self.config.get_value("list-scenes", "skip-cuts")
+        self.list_scenes_quiet = quiet or self.config.get_value("list-scenes", "quiet")
+        no_output_file = no_output_file or self.config.get_value("list-scenes", "no-output-file")
 
         self.scene_list_directory = self.config.get_value(
-            'list-scenes', 'output', output, ignore_default=True)
-        self.scene_list_name_format = self.config.get_value('list-scenes', 'filename', filename)
+            "list-scenes", "output", output, ignore_default=True)
+        self.scene_list_name_format = self.config.get_value("list-scenes", "filename", filename)
         if self.scene_list_name_format is not None and not no_output_file:
-            logger.info('Scene list filename format:\n  %s', self.scene_list_name_format)
+            logger.info("Scene list filename format:\n  %s", self.scene_list_name_format)
         self.scene_list_output = not no_output_file
         if self.scene_list_directory is not None:
-            logger.info('Scene list output directory:\n  %s', self.scene_list_directory)
+            logger.info("Scene list output directory:\n  %s", self.scene_list_directory)
 
         self.list_scenes = True
 

scenedetect/_cli/controller.py
@@ -154,24 +154,24 @@ def _list_scenes(context: CliContext, scene_list: List[Tuple[FrameTimecode, Fram
                 scene_list=scene_list,
                 include_cut_list=not context.skip_cuts,
                 cut_list=cut_list)
-
-    if context.print_scene_list:
-        logger.info(
-            """Scene List:
+    if not context.list_scenes_quiet:
+        if context.display_scenes:
+            logger.info(
+                """Scene List:
 -----------------------------------------------------------------------
  | Scene # | Start Frame |  Start Time  |  End Frame  |   End Time   |
 -----------------------------------------------------------------------
 %s
 -----------------------------------------------------------------------""", '\n'.join([
-                ' |  %5d  | %11d | %s | %11d | %s |' %
-                (i + 1, start_time.get_frames() + 1, start_time.get_timecode(),
-                 end_time.get_frames(), end_time.get_timecode())
-                for i, (start_time, end_time) in enumerate(scene_list)
-            ]))
+                    " |  %5d  | %11d | %s | %11d | %s |" %
+                    (i + 1, start_time.get_frames() + 1, start_time.get_timecode(),
+                     end_time.get_frames(), end_time.get_timecode())
+                    for i, (start_time, end_time) in enumerate(scene_list)
+                ]))
 
-        if cut_list:
-            logger.info('Comma-separated timecode list:\n  %s',
-                        ','.join([cut.get_timecode() for cut in cut_list]))
+        if cut_list and context.display_cuts:
+            logger.info("Comma-separated timecode list:\n  %s",
+                        ",".join([cut.get_timecode() for cut in cut_list]))
 
 
 def _save_images(

scenedetect/_cli/config.py
@@ -263,9 +263,11 @@ CONFIG_MAP: ConfigDict = {
         'no-images': False,
     },
     'list-scenes': {
-        'output': '',
+        'display-cuts': True,
+        'display-scenes': True,
         'filename': '$VIDEO_NAME-Scenes.csv',
-        'no-output-file': False,
+        'output': '',
+        'no-output-file': False,                                                 # TODO(v0.6.3): Rename this to 'save'.
         'quiet': False,
         'skip-cuts': False,
     },

commit 65ce4a654b3f043b94821469d90b15d1b6f118fe

[cli] Fix scene list always being printed
Regressed in 46e170e02a2eea09808619a8d004852123a651c5

scenedetect/_cli/context.py
@@ -156,9 +156,9 @@ class CliContext:
         self.list_scenes_quiet: bool = None                       # list-scenes -q/--quiet
         self.scene_list_dir: str = None                           # list-scenes -o/--output
         self.scene_list_name_format: str = None                   # list-scenes -f/--filename
-        self.scene_list_output: bool = None                       # list-scenes -n/--no-output
+        self.scene_list_output: bool = None                       # list-scenes -n/--no-output-file
         self.skip_cuts: bool = None                               # list-scenes -s/--skip-cuts
-        self.display_cuts: bool = False                           # [list-scenes] display-cuts
+        self.display_cuts: bool = True                            # [list-scenes] display-cuts
         self.display_scenes: bool = True                          # [list-scenes] display-scenes
         self.cut_format: TimecodeFormat = TimecodeFormat.TIMECODE # [list-scenes] cut-format
 

scenedetect/_cli/controller.py
@@ -139,6 +139,9 @@ def _save_stats(context: CliContext) -> None:
 def _list_scenes(context: CliContext, scene_list: List[Tuple[FrameTimecode, FrameTimecode]],
                  cut_list: List[FrameTimecode]) -> None:
     """Handles the `list-scenes` command."""
+    if not context.list_scenes:
+        return
+    # Write scene list CSV to if required.
     if context.scene_list_output:
         scene_list_filename = Template(
             context.scene_list_name_format).safe_substitute(VIDEO_NAME=context.video_stream.name)
@@ -154,24 +157,27 @@ def _list_scenes(context: CliContext, scene_list: List[Tuple[FrameTimecode, Fram
                 scene_list=scene_list,
                 include_cut_list=not context.skip_cuts,
                 cut_list=cut_list)
-    if not context.list_scenes_quiet:
-        if context.display_scenes:
-            logger.info(
-                """Scene List:
+    # Suppress output if requested.
+    if context.list_scenes_quiet:
+        return
+    # Print scene list.
+    if context.display_scenes:
+        logger.info(
+            """Scene List:
 -----------------------------------------------------------------------
- | Scene # | Start Frame |  Start Time  |  End Frame  |   End Time   |
+| Scene # | Start Frame |  Start Time  |  End Frame  |   End Time   |
 -----------------------------------------------------------------------
 %s
 -----------------------------------------------------------------------""", '\n'.join([
-                    " |  %5d  | %11d | %s | %11d | %s |" %
-                    (i + 1, start_time.get_frames() + 1, start_time.get_timecode(),
-                     end_time.get_frames(), end_time.get_timecode())
-                    for i, (start_time, end_time) in enumerate(scene_list)
-                ]))
-
-        if cut_list and context.display_cuts:
-            logger.info("Comma-separated timecode list:\n  %s",
-                        ",".join([context.cut_format.format(cut) for cut in cut_list]))
+                " |  %5d  | %11d | %s | %11d | %s |" %
+                (i + 1, start_time.get_frames() + 1, start_time.get_timecode(),
+                 end_time.get_frames(), end_time.get_timecode())
+                for i, (start_time, end_time) in enumerate(scene_list)
+            ]))
+    # Print cut list.
+    if cut_list and context.display_cuts:
+        logger.info("Comma-separated timecode list:\n  %s",
+                    ",".join([context.cut_format.format(cut) for cut in cut_list]))
 
 
 def _save_images(